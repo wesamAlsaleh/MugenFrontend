dc0645b6f6da0c739f2de4294072c9092d55d939
wesamAlsaleh <wesammuneer7@gmail.com>
2025-09-11 15:45:56 +0300

    feat: add the tab bar

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text, StyleSheet } from "react-native";
+
+export default function index() {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#f4511e",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
