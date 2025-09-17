dbeec42c7bbcc30e812ee2ef41d2ba2e1c715c7a
wesamAlsaleh <wesammuneer7@gmail.com>
2025-09-17 20:14:40 +0300

    chore: install react-native-element-dropdown dependancy

--- app/(tabs)/list.tsx
+++ app/(tabs)/list.tsx
@@ -1,9 +1,24 @@
+import { useTheme } from "@/hooks/use-theme";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function ListScreen() {
+  // Get the theme
+  const theme = useTheme();
+
+  // Dynamic styles based on the theme
+  const dynamicStyles = {
+    container: {
+      backgroundColor: theme.backgroundColor,
+    },
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>My List</Text>
+    <View style={[styles.container, dynamicStyles.container]}>
+      {/* Status Selector */}
+      <View></View>
+
+      {/* Results Selector */}
+      <Text style={{ color: theme.primaryText }}>My List</Text>
     </View>
   );
 }
